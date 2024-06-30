005f9c39c991cc03d523d6b2b4634d0975e39c8b
fajarrmaulanna <fajarrmaulanna@gmail.com>
2024-07-01 01:17:49 +0700

    feat (auth): implement authentication middleware and role-based authorization to CategoryRoutes

--- src/routes/CategoryRoutes.ts
+++ src/routes/CategoryRoutes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory } from "../controllers/CategoryController";
-
+import { AuthCheck, AuthCheckRole } from "../middleware/AuthCheck";
 
 const router = Router();
 
-router.post('/', createCategory);
-router.get('/', getAllCategories);
-router.get('/:id', getCategoryById);
-router.put('/:id', updateCategory);
-router.delete('/:id', deleteCategory);
+router.post('/', AuthCheck, AuthCheckRole(["organizer"]), createCategory);
+router.get('/', AuthCheck, AuthCheckRole(["organizer", "admin"]), getAllCategories);
+router.get('/:id', AuthCheck, AuthCheckRole(["organizer", "admin"]), getCategoryById);
+router.put('/:id', AuthCheck, AuthCheckRole(["organizer"]), updateCategory);
+router.delete('/:id', AuthCheck, AuthCheckRole(["organizer"]), deleteCategory);
 
 export default router;
