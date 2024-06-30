ae8207bec9a073c730a527c92d22611767e24bb3
fajarrmaulanna <fajarrmaulanna@gmail.com>
2024-07-01 01:20:39 +0700

    update : update index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,15 @@
 import { Router } from "express";
-import { AuthCheck } from "../middleware/AuthCheck";
 import AuthRoutes from "./AuthRoutes";
 import CategorRoutes from "./CategoryRoutes";
 import EventRoutes from "./EventRoutes";
 import ImageRoutes from "./ImageRoutes";
 import OrganizerRoutes from "./OrganizerRoutes";
 import TalentRoutes from "./TalentRoutes";
-import UserRoutes from "./UserRoutes";
+// import UserRoutes from "./UserRoutes";
 
 const routes = Router();
 
-routes.use("/api/v1/users", AuthCheck, UserRoutes);
+// routes.use("/api/v1/users", AuthCheck, UserRoutes);
 routes.use("/api/v1/categories", CategorRoutes);
 routes.use("/api/v1/auth", AuthRoutes)
 routes.use("/api/v1/images", ImageRoutes)
