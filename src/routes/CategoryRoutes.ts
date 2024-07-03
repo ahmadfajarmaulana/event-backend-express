b426fc0475db0704e28c052185b98c81a142886e
fajarrmaulanna <fajarrmaulanna@gmail.com>
2024-07-03 22:38:22 +0700

    update (categories/route): remove admin on AuthCheckRole (organizer only)

--- src/routes/CategoryRoutes.ts
+++ src/routes/CategoryRoutes.ts
@@ -5,8 +5,8 @@ import { AuthCheck, AuthCheckRole } from "../middleware/AuthCheck";
 const router = Router();
 
 router.post('/', AuthCheck, AuthCheckRole(["organizer"]), createCategory);
-router.get('/', AuthCheck, AuthCheckRole(["organizer", "admin"]), getAllCategories);
-router.get('/:id', AuthCheck, AuthCheckRole(["organizer", "admin"]), getCategoryById);
+router.get('/', AuthCheck, AuthCheckRole(["organizer"]), getAllCategories);
+router.get('/:id', AuthCheck, AuthCheckRole(["organizer"]), getCategoryById);
 router.put('/:id', AuthCheck, AuthCheckRole(["organizer"]), updateCategory);
 router.delete('/:id', AuthCheck, AuthCheckRole(["organizer"]), deleteCategory);
 
