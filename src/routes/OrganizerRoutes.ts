b0d4bf879f0fef41aa781bb87f56be19bf006ba4
fajarrmaulanna <fajarrmaulanna@gmail.com>
2024-07-04 00:26:07 +0700

    update (organizer/route): add AuthCheck and AuthCheckRole (owner only) on create organizer Route

--- src/routes/OrganizerRoutes.ts
+++ src/routes/OrganizerRoutes.ts
@@ -5,7 +5,7 @@ import { AuthCheck, AuthCheckRole } from "../middleware/AuthCheck";
 
 const router = Router();
 
-router.post('/', createOrganizer);
+router.post('/', AuthCheck, AuthCheckRole(["owner"]), createOrganizer);
 router.post('/admin', AuthCheck, AuthCheckRole(["organizer"]), createAdmin);
 
 export default router;
